964fd8bc52e9637346336b34048a58efd2d8dc03
Joshua Holman <joshuaholman5@gmail.com>
2024-02-24 21:00:56 -0800

    added license

--- Task.h
+++ Task.h
@@ -1,4 +1,26 @@
-// Task.h
+/*
+ * Project: task
+ * File: Task.h
+ * Description: abstracts tasks into an object
+ * Copyright (C) 2024 Joshua Holman
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Joshua Holman
+ * Contact: jolman@duck.com
+ */
+
 #pragma once
 #include <string>
 #include "Date.h"
