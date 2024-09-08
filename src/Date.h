47d8c3e7ede11b8ae22d61d157970a8718ef621b
josh <joshuaholman@gmail.com>
2024-09-07 22:25:46 -0700

    restructured Task class

--- src/Date.h
+++ src/Date.h
@@ -1,7 +1,8 @@
 /**
  * @file Date.h
- * @brief Handles two dates
+ * @brief Interface for the Date class
  * @author Joshua Holman
+ * @contact jolman@duck.com
  * 
  * @copyright Copyright (c) 2024 Joshua Holman
  * 
@@ -15,83 +16,62 @@
 #include <sstream>
 #include <iomanip>
 #include <iostream>
-#include <sstream>
 
 /**
  * @brief A class that uses chrono to handle due dates
- * @author Joshua Holman
- * @date 2024-09-07
  *
- * This class provides string input and output for dates. It handles two dates:
- * the current date, which is initialized at object creation, and a future date
- * provided by the user. Includes methods for validating dates and calculating
- * the difference between the two.
+ * This class provides can parse strings to create dates and create string
+ * representations for output. It handles two dates: the current date, which
+ * is initialized at object creation, and a future date provided by the user.
+ * Includes methods for validating dates and calculating the difference between
+ * the two.
  */
 class Date {
 public:
   /**
-   * @brief Constructs a date object with the same future time as current
+   * @brief Constructs a date object with the time the object was created
    */
-  Date() noexcept : currentDateTime(std::chrono::system_clock::now()), 
-              futureDateTime(std::chrono::system_clock::now()) {}
+  Date() noexcept : date(std::chrono::system_clock::now()) {}
 
   /**
-   * @brief Constructs a date object with provided future date
-   * @param dateStr Future date provided as a string
+   * @brief Constructs a date object with provided date
+   * @param dateStr date provided as a string
    */
-  Date(const std::string& dateStr) : currentDateTime(std::chrono::system_clock::now()),
-              futureDateTime(parseDate(dateStr)) {}
+  Date(const std::string& dateStr) : date(parseDate(dateStr)) {}
 
   /**
    * @brief Compares two date objects. Used for sorting 
    */
   bool operator<(const Date& other) const {
-      return futureDateTime < other.futureDateTime;
+      return date < other.date;
   }
+
+  /**
+   * @brief Overloaded subraction to get the diff between two dates in hours
+   */
+  std::chrono::hours operator-(const Date& other) const {
+    return std::chrono::duration_cast<std::chrono::hours>(date-other.date);
+  }
+
+  /**
+   * @brief Converts Date.futureDate to a string
+   */
+  static std::string toString(const Date& date) noexcept;
 
   /**
    * @brief Used for printing date objects with std::cout
    */
-  friend std::ostream& operator<<(std::ostream& os, const Date& d) {
-    std::time_t tt = std::chrono::system_clock::to_time_t(d.futureDateTime);
-    tm local_tm = *localtime(&tt);
-    os << std::put_time(&local_tm, outputFormatStr);
-    return os;
-}
+  friend std::ostream& operator<<(std::ostream& os, const Date& d) noexcept;
 
 private:
-  std::chrono::system_clock::time_point futureDateTime;
-  std::chrono::system_clock::time_point currentDateTime;
-  const std::string inputFormatStr = "%m/%d";
-  const std::string outputFormatStr = "%m/%d";
+  std::chrono::system_clock::time_point date;
+  static const std::string inputFormatStr;
+  static const std::string outputFormatStr;
 
-
-  bool isValidDate(int day, int month, int year) {
-    if(year < 0 || year > 9999 || month < 1 || month > 12)
-        return false;
-    if(day < 1 || day > 31)
-        return false;
-    if(month == 2) {
-        bool isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        if(day > 29 || (day == 29 && !isLeap))
-            return false;
-    }
-    if(day == 31 && (month == 4 || month == 6 || month == 9 || month == 11))
-        return false;
-    return true;
-  }
-
-  std::chrono::system_clock::time_point parseDate(const std::string& dateString) {
-    std::tm tm = {};
-    std::istringstream ss(dateString);
-    ss >> std::get_time(&tm, inputFormatStr.c_str());
-
-    if (ss.fail()) {
-      throw std::runtime_error("Date parse failed");
-    }
-
-    return std::chrono::system_clock::from_time_t(std::mktime(&tm));
-  }
-
+  /**
+   * @briefUsed in the constructor to convert date strings to std::chrono::system_clock::time_point
+   * @param dateString User provided date string in the form of mm/dd
+   */
+  std::chrono::system_clock::time_point parseDate(const std::string& dateString);
   
 };
--- src/Task.h
+++ src/Task.h
@@ -1,94 +1,55 @@
-/*
- * Project: task
- * File: Task.h
- * Description: abstracts tasks into an object
- * Copyright (C) 2024 Joshua Holman
+/**
+ * @file Task.h
+ * @brief Interface for the Task class
+ * @author Joshua Holman
+ * @contact jolman@duck.com
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * @copyright Copyright (c) 2024 Joshua Holman
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Author: Joshua Holman
- * Contact: jolman@duck.com
+ * This source code is licensed under the MIT License.
+ * See the LICENSE file in the project root for full license information.
  */
 
 #pragma once
 #include <string>
-#include "Date.hpp"
+#include "Date.h"
 #include <sstream>
 #include <iostream>
 
+/**
+ * @brief A class abstracts Tasks with a name, due date, and time to complete
+ *        and completion status
+ *
+ * This class provides can parse strings to create dates and create string
+ * representations for output. It handles two dates: the current date, which
+ * is initialized at object creation, and a future date provided by the user.
+ * Includes methods for validating dates and calculating the difference between
+ * the two.
+ */
 class Task {
+public:
+
+  Task(std::string name) : name(name), completionStatus(false) {
+    timeUntilDue = std::chrono::hours(0);
+  }
+
+  Task(std::string name, Date dueDateStr) : name(name), completionStatus(false), dueDate(Date(dueDateStr)) {
+    Date initialDate;
+    timeUntilDue = dueDate - initialDate;
+  }
+
+  void markComplete() { completionStatus = true; }
+
+  std::string getName() { return this->name; }
+  bool isComplete() { return this->completionStatus; }
+  Date getDueDate() { return this->dueDate; }
+  int getTimeUntilDue() { return this->timeUntilDue.count(); } // maybe needs to be long long
+
 private:
 
   std::string name;
+  bool completionStatus;
   Date dueDate; 
-  bool completion;
-  Date currentDate;
+  std::chrono::hours timeUntilDue;
 
-public:
-
-  Task(std::string name, Date dueDate)
-    : name(name), dueDate(dueDate), completion(false), currentDate(Date()) {
-}
-  Task(std::string name, Date dueDate, bool completion, Date currentDate)
-    : name(name), dueDate(dueDate), completion(completion), currentDate(currentDate) {}
-
-  void markComplete() { completion = true; }
-
-  bool getCompletion() const { return completion; }
-  
-  std::string getName() const { return name; }
-
-  friend std::ostream& operator<<(std::ostream& os, const Task& t) {
-    std::string status = (t.completion ? "Completed" : "Incomplete");
-    os << t.name;
-    if (t.dueDate.toString() != t.currentDate.toString()) { // for when no due date is provided
-      os << "\t" << t.dueDate;
-    } else {
-      os << "\t\t";
-    }
-
-    if (!t.completion && t.isOverdue()) {
-      os << "\t\033[31m" << status << "\033[0m"; // red if overdue
-    } else {
-      os << "\t\033[32m" << status << "\033[0m"; // green if not overdue
-    }
-
-    return os;
-  }
-
-  std::string serialize() const {
-    std::ostringstream oss;
-    oss << name << ',' << dueDate << ',' << completion << ',' << currentDate;
-    return oss.str();
-  }
-
-  static Task deserialize(const std::string &data) {
-    std::istringstream iss(data);
-    std::string token;
-    std::getline(iss, token, ',');
-    std::string name = token;
-    std::getline(iss, token, ',');
-    Date dueDate(token);
-    std::getline(iss, token, ',');
-    bool completion = (token == "1");
-    std::getline(iss, token, ',');
-    Date currentDate(token);
-    return Task(name, dueDate, completion, currentDate);
-  }
-
-  // for displaying overdue tasks in red
-  bool isOverdue() const {
-    return dueDate < currentDate;
-  }
 };
