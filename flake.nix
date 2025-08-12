4d748ffa1e1dc6ad02dde6c3001f8432cb591216
Josh Holman <josh@jolman.me>
2025-08-12 16:05:34 -0700

    set CXX

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
               make clean && bear -- make
             fi
 
-            export CC="clang++"
+            export CXX="clang++"
             export PATH="$PWD/build:$PATH"
           '';
         };
